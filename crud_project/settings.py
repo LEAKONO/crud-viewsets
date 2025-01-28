f8c24a555995cd334894f08a37e5497d46321381
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2025-01-28 13:33:08 +0300

    hurray created api

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'items'
+    'rest_framework',
+    'items',
 ]
 
 MIDDLEWARE = [
